f1c7d776f39f80c850774f4b39272a8f52ad3a2c
rahul <rahulsharma6660357@gmail.com>
2019-09-24 13:33:47 +0530

    cards and add details to cards with json file

---
+++ src/app/Details.ts
@@ -0,0 +1,42 @@
+export const Details = [
+  {
+    name: 'Phone XL',
+    price: 18000,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 18999,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 20099,
+    description: 'the best phone in the world'
+  },
+  {
+    name: 'Samsung',
+    price: 19999,
+    description: 'long battery life '
+  },
+  {
+    name: 'Sony',
+    price: 25000,
+    description: 'the best camera'
+  },
+  {
+    name: 'Addidas',
+    price: 25000,
+    description: 'the best camera'
+  },
+  {
+    name: 'PUMA',
+    price: 25000,
+    description: 'the best camera'
+  },
+  {
+    name: 'Reebok',
+    price: 25000,
+    description: ''
+  }
+];
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Details} from '../Details';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  details = Details;
 
   constructor() { }
 
